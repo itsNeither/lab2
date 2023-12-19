12e34117c6ad9251cb96bb98adffa24f7041af5c
Bulychev Nikita <62388618+itsNeither@users.noreply.github.com>
2023-12-19 23:09:30 +0500

    Add files via upload

---
+++ LAB2BULYCHEV.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russia");
+	int n = 0;
+	while (n <= 3 || n % 2 == 0) {
+		cout << "Введите n(n > 3 и n - нечетное): ";
+		cin >> n;
+	}
+	for (int i = 1; i <= n; i += 2) {
+		for (int j = 0; j < (n - i) / 2; j++) cout << " ";
+		for (int j = 0; j < i; j++) cout << "*";
+		cout << endl;
+	}
+	return 0;
+}
+
